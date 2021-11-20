8d607472cda60c994c99dd38000f58b3d6437393
garyopen1876 <garyopen1876@gmail.com>
2021-11-20 13:02:04 +0800

    Rebuild首頁成功
    1. main.css修復
    2. 部分html code修改
    3.migrate web 的 資料庫

---
+++ Industry_Academy/local_settings.py
@@ -0,0 +1,10 @@
+DEBUG = True
+# DEBUG = False
+
+
+virtualenv_path = '/home/zs124927755/venv/lib/python3.6/site-packages'
+LOGGING_FOLDER = 'Industry_Academy/logs'
+# nid client_id
+NID_CLIENT_ID = '637178106413.30124fe008d0449f9695cc94b7613ede.140.134.24.239'
+
+
